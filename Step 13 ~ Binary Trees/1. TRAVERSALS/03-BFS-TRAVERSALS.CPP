c98a414af73346bab9f491fb275679fa8940292e
Dhruv Yadav <dhruv.nitj@gmail.com>
2023-07-29 19:42:42 +0530

    updates Trees

---
+++ Step 13 ~ Binary Trees/1. TRAVERSALS/03-BFS-TRAVERSALS.CPP
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+
+        vector<vector<int>> ans ; if(!root){return ans ;}
+
+        queue<TreeNode*> q ; q.push(root) ;
+
+        while(!q.empty()) {
+
+            int size = q.size() ; vector<int> temp;
+            for(int i =0 ; i < size ; i++) {
+                auto it = q.front() ; q.pop() ; temp.push_back(it -> val) ;
+
+                if(it -> left) {q.push(it -> left) ;}
+                if(it -> right) {q.push(it -> right) ;}
+            }
+
+            ans.push_back(temp) ;
+
+        }
+        return ans ;
+    }
+};
